5b24f5ff254eb159a0db3f6ca7d707609dd79d73
Kin Lane <info@apievangelist.com>
2015-10-29 18:08:26 -0700

    make it return short url on add

--- api/methods/m-link-post.php
+++ api/methods/m-link-post.php
@@ -12,7 +12,7 @@ $app->post($route, function () use ($app){
 	if(isset($params['url'])){ $url = mysql_real_escape_string($params['url']); } else { $url = 'no url'; }
 	if(isset($params['short_url'])){ $short_url = mysql_real_escape_string($params['short_url']); } else { $short_url = "http://apis.how/" . strtolower(getToken(10)); }
 
-  	$Query = "SELECT * FROM link WHERE url = '" . $url . "'";
+  $Query = "SELECT * FROM link WHERE url = '" . $url . "'";
 	//echo $Query . "<br />";
 	$Database = mysql_query($Query) or die('Query failed: ' . mysql_error());
 
@@ -24,45 +24,45 @@ $app->post($route, function () use ($app){
 	else
 		{
 		$short_url = "http://apis.how/" . strtolower(getToken(10));
-		
+
 		// Check 3 times if unique (funcationalize this)
-		$short_url_exist = 0;	
+		$short_url_exist = 0;
 	  	$shortquery = "SELECT * FROM link WHERE url = '" . $url . "'";
 		$shortqueryresult = mysql_query($shortquery) or die('Query failed: ' . mysql_error());
 		if($shortqueryresult && mysql_num_rows($shortqueryresult))
 			{
 			$short_url_exist = 1;
 			$short_url = "http://apis.how/" . strtolower(getToken(10));
-			$short_url_exist = 0;	
+			$short_url_exist = 0;
 		  	$shortquery = "SELECT * FROM link WHERE url = '" . $url . "'";
 			$shortqueryresult = mysql_query($shortquery) or die('Query failed: ' . mysql_error());
 			if($shortqueryresult && mysql_num_rows($shortqueryresult))
 				{
 				$short_url_exist = 1;
 				$short_url = "http://apis.how/" . strtolower(getToken(10));
-				$short_url_exist = 0;	
+				$short_url_exist = 0;
 			  	$shortquery = "SELECT * FROM link WHERE url = '" . $url . "'";
 				$shortqueryresult = mysql_query($shortquery) or die('Query failed: ' . mysql_error());
 				if($shortqueryresult && mysql_num_rows($shortqueryresult))
 					{
 					$short_url_exist = 1;
 					$short_url = "http://apis.how/" . strtolower(getToken(10));
-					}	
-				else 
-					{
-					$short_url_exist = 0;	
 					}
-				}	
-			else 
+				else
+					{
+					$short_url_exist = 0;
+					}
+				}
+			else
 				{
-				$short_url_exist = 0;	
-				}						
+				$short_url_exist = 0;
+				}
 			}
-		else 
+		else
 			{
-			$short_url_exist = 0;	
-			}		
-			
+			$short_url_exist = 0;
+			}
+
 		$Query = "INSERT INTO link(created_date,url,short_url)";
 		$Query .= " VALUES(";
 		$Query .= "'" . mysql_real_escape_string($created_date) . "',";
@@ -78,6 +78,7 @@ $app->post($route, function () use ($app){
    	$link_id = prepareIdOut($link_id,$host);
 
 	$ReturnObject['link_id'] = $link_id;
+	$ReturnObject['short_url'] = $short_url;
 
 	$app->response()->header("Content-Type", "application/json");
 	echo format_json(json_encode($ReturnObject));
