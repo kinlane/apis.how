8acd880d30668c7af1fe02a55f633740d220100d
Kin Lane <info@apievangelist.com>
2016-01-01 19:03:26 -0800

    add some logic

--- app/index.php
+++ app/index.php
@@ -14,9 +14,9 @@ require_once('/var/www/html/system/class-amazon-s3.php');
 $url = "";
 $Requested_URL = "http://apis.how" . $_SERVER['REQUEST_URI'];
 
-echo $Requested_URL . "<br />";
+//echo $Requested_URL . "<br />";
 
-if(strlen($Requested_URL) > 4)
+if(strlen($Requested_URL) > 4 && $Requested_URL !='http://apis.how/'
   {
   $Query = "SELECT * FROM link WHERE short_url = '" . $Requested_URL . "'";
   $DatabaseResult = mysql_query($Query) or die('Query failed: ' . mysql_error());
@@ -27,35 +27,41 @@ if(strlen($Requested_URL) > 4)
     $url = $Database['url'];
     $short_url = $Database['short_url'];
     }
-  //echo $url;
-echo $link_id . "<br />";
-  $table_name = "track_url_" . $link_id;
 
-  $checkLikeTableQuery = "show tables from `stack_network_kinlane_apishow` like " . chr(34) . $table_name . chr(34);
-  $checkLikeTableResult = mysql_query($checkLikeTableQuery) or die('Query failed: ' . mysql_error());
-
-  if($checkLikeTableResult && mysql_num_rows($checkLikeTableResult))
+  if(isset($link_id))
     {
-    $checkLikeTableResult = mysql_fetch_assoc($checkLikeTableResult);
+    $table_name = "track_url_" . $link_id;
+
+    $checkLikeTableQuery = "show tables from `stack_network_kinlane_apishow` like " . chr(34) . $table_name . chr(34);
+    $checkLikeTableResult = mysql_query($checkLikeTableQuery) or die('Query failed: ' . mysql_error());
+
+    if($checkLikeTableResult && mysql_num_rows($checkLikeTableResult))
+      {
+      $checkLikeTableResult = mysql_fetch_assoc($checkLikeTableResult);
+      }
+    else
+      {
+      $CreateTableQuery = "CREATE TABLE  `stack_network_kinlane_apishow`.`" . $table_name . "` (";
+      $CreateTableQuery .= "`track_id` int(10) unsigned NOT NULL AUTO_INCREMENT,";
+      $CreateTableQuery .= "`click_date` datetime NOT NULL,";
+      $CreateTableQuery .= "PRIMARY KEY (`track_id`)";
+      $CreateTableQuery .= ") ENGINE=InnoDB DEFAULT CHARSET=latin1;  ";
+      //echo "<br />" . $CreateTableQuery . "<br />";
+      mysql_query($CreateTableQuery) or die('Query failed: ' . mysql_error());
+
+      }
+
+    $click_date = date('Y-m-d H:i:s');
+    $query = "INSERT INTO " . $table_name . "(click_date) VALUES('" . mysql_real_escape_string($click_date) . "')";
+    //echo $query . "<br />";
+    mysql_query($query) or die('Query failed: ' . mysql_error());
+
+    header('Location: ' . $url);
     }
   else
     {
-    $CreateTableQuery = "CREATE TABLE  `stack_network_kinlane_apishow`.`" . $table_name . "` (";
-    $CreateTableQuery .= "`track_id` int(10) unsigned NOT NULL AUTO_INCREMENT,";
-    $CreateTableQuery .= "`click_date` datetime NOT NULL,";
-    $CreateTableQuery .= "PRIMARY KEY (`track_id`)";
-    $CreateTableQuery .= ") ENGINE=InnoDB DEFAULT CHARSET=latin1;  ";
-    //echo "<br />" . $CreateTableQuery . "<br />";
-    mysql_query($CreateTableQuery) or die('Query failed: ' . mysql_error());
-
+    echo '<br /><br /><br /><p align="center">apis.how</p>';
     }
-
-  $click_date = date('Y-m-d H:i:s');
-  $query = "INSERT INTO " . $table_name . "(click_date) VALUES('" . mysql_real_escape_string($click_date) . "')";
-  //echo $query . "<br />";
-  mysql_query($query) or die('Query failed: ' . mysql_error());
-
-  header('Location: ' . $url);
   }
 else
   {
